d27deb499bdb75938e15b3371c73372401ca2329
Ash Wilson <smashwilson@gmail.com>
2014-12-16 12:58:01 -0500

    Standardize the way that API errors are reported.

--- frontdoor/main.go
+++ frontdoor/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -34,4 +35,34 @@ type ContextHandler func(c *Context, w http.ResponseWriter, r *http.Request)
 // BindContext returns an http.HandlerFunc that binds a ContextHandler to a specific Context.
 func BindContext(c *Context, handler ContextHandler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) { handler(c, w, r) }
+}
+
+// RhoError stores information that may be returned in an error response from the API.
+type RhoError struct {
+	Code    string `json:"code"`
+	Message string `json:"message"`
+	Hint    string `json:"hint,omitempty"`
+	Retry   bool   `json:"retry,omitempty"`
+}
+
+// Report serializes an error report as JSON to an open ResponseWriter.
+func (e RhoError) Report(status int, w http.ResponseWriter) error {
+	var outer struct {
+		Error RhoError `json:"error"`
+	}
+	outer.Error = e
+
+	b, err := json.Marshal(outer)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("Unable to serialize API error.")
+		fmt.Fprintf(w, "Er, there was an error serializing the error. Talk to your administrator, please.")
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	_, err = w.Write(b)
+	return err
 }
