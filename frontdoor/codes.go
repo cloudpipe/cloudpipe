064cb6d359862cfb85d2c6dfd27fe9e66d86feda
Ash Wilson <smashwilson@gmail.com>
2014-12-18 10:54:00 -0500

    Start centralizing error codes.

---
+++ frontdoor/codes.go
@@ -0,0 +1,10 @@
+package main
+
+const (
+	// CodeMissingCommand means a job is missing a "cmd" element.
+	CodeMissingCommand = "CMD"
+	// CodeInvalidResultSource means a job has an invalid result source.
+	CodeInvalidResultSource = "RSRC"
+	// CodeInvalidResultType means a job has an invalid result type.
+	CodeInvalidResultType = "RTYPE"
+)
