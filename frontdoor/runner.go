5a077a2590e9a04b621a739270879bfa20341d27
Ash Wilson <smashwilson@gmail.com>
2014-12-19 09:56:34 -0500

    Create a dummy job runner.

--- frontdoor/runner.go
+++ frontdoor/runner.go
@@ -1,8 +1,49 @@
 package main
 
-import "fmt"
+import (
+	"time"
+
+	log "github.com/Sirupsen/logrus"
+)
 
 // Runner is the main entry point for the job runner goroutine.
 func Runner(c *Context) {
-	fmt.Println("Look at me run, run run run.")
+	for {
+		select {
+		case <-time.After(time.Duration(c.Poll) * time.Millisecond):
+			Claim(c)
+		}
+	}
+}
+
+// Claim acquires the oldest single pending job and launches a goroutine to execute its command in
+// a new container.
+func Claim(c *Context) {
+	job, err := c.ClaimJob()
+	if err != nil {
+		log.WithFields(log.Fields{
+			"error": err,
+		}).Error("Unable to claim a job.")
+		return
+	}
+	if job == nil {
+		// Nothing to claim.
+		return
+	}
+
+	log.WithFields(log.Fields{
+		"jid": job.JID,
+	}).Info("Launching a new job.")
+
+	go Execute(c, job)
+}
+
+// Execute launches a container to process the submitted job. It passes any provided stdin data
+// to the container and consumes stdout and stderr, updating Mongo as it runs. Once completed, it
+// acquires the job's result from its configured source and marks the job as finished.
+func Execute(c *Context, job *SubmittedJob) {
+	log.WithFields(log.Fields{
+		"jid":     job.JID,
+		"command": job.Command,
+	}).Info("Hey look I'm executing a job!")
 }
