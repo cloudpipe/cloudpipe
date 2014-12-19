a760149b175276e62475990e5d7faa512420c515
Ash Wilson <smashwilson@gmail.com>
2014-12-19 10:39:45 -0500

    Connect to Docker.

--- frontdoor/runner.go
+++ frontdoor/runner.go
@@ -4,21 +4,31 @@ import (
 	"time"
 
 	log "github.com/Sirupsen/logrus"
+	docker "github.com/fsouza/go-dockerclient"
 )
 
 // Runner is the main entry point for the job runner goroutine.
 func Runner(c *Context) {
+	client, err := docker.NewClient(c.DockerHost)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"docker host": c.DockerHost,
+			"error":       err,
+		}).Fatal("Unable to connect to Docker.")
+		return
+	}
+
 	for {
 		select {
 		case <-time.After(time.Duration(c.Poll) * time.Millisecond):
-			Claim(c)
+			Claim(c, client)
 		}
 	}
 }
 
 // Claim acquires the oldest single pending job and launches a goroutine to execute its command in
 // a new container.
-func Claim(c *Context) {
+func Claim(c *Context, client *docker.Client) {
 	job, err := c.ClaimJob()
 	if err != nil {
 		log.WithFields(log.Fields{
@@ -35,13 +45,13 @@ func Claim(c *Context) {
 		"jid": job.JID,
 	}).Info("Launching a new job.")
 
-	go Execute(c, job)
+	go Execute(c, client, job)
 }
 
 // Execute launches a container to process the submitted job. It passes any provided stdin data
 // to the container and consumes stdout and stderr, updating Mongo as it runs. Once completed, it
 // acquires the job's result from its configured source and marks the job as finished.
-func Execute(c *Context, job *SubmittedJob) {
+func Execute(c *Context, client *docker.Client, job *SubmittedJob) {
 	job.StartedAt = StoreTime(time.Now())
 	if err := c.UpdateJob(job); err != nil {
 		log.WithFields(log.Fields{
