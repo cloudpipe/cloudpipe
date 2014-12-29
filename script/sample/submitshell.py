1a4d15ca137ad9c09fc474500303142e118335ff
Ash Wilson <smashwilson@gmail.com>
2014-12-29 15:19:04 -0500

    Change the sample command.

--- script/sample/submitshell.py
+++ script/sample/submitshell.py
@@ -4,7 +4,7 @@ import multyvac
 
 multyvac.config.set_key(api_key='admin', api_secret_key='12345', api_url='http://docker:8000/api')
 
-jid = multyvac.shell_submit(cmd='id')
+jid = multyvac.shell_submit(cmd='for i in {1..10}; do echo $i && sleep 10; done')
 print("Submitted job [{}].".format(jid))
 
 job = multyvac.get(jid)
