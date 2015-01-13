11adbb947a3be62dde52db893b9986ca5b295a94
Ash Wilson <smashwilson@gmail.com>
2015-01-13 08:06:29 -0500

    Improve authentication tests.

--- auth_test.go
+++ auth_test.go
@@ -6,6 +6,14 @@ import (
 	"testing"
 )
 
+// TrustingAuthService accepts all usernames and tokens.
+type TrustingAuthService struct{}
+
+// Validate always returns true.
+func (service TrustingAuthService) Validate(username, token string) (bool, error) {
+	return true, nil
+}
+
 func setupAuthRecorder(t *testing.T, username, key string) (*http.Request, *httptest.ResponseRecorder) {
 	r, err := http.NewRequest("GET", "https://localhost/v1/jobs", nil)
 	if err != nil {
@@ -21,7 +29,8 @@ func setupAuthRecorder(t *testing.T, username, key string) (*http.Request, *http
 func TestAuthenticateMissingCredentials(t *testing.T) {
 	r, w := setupAuthRecorder(t, "", "")
 	c := &Context{
-		Storage: NullStorage{},
+		Storage:     NullStorage{},
+		AuthService: NullAuthService{},
 	}
 
 	_, err := Authenticate(c, w, r)
@@ -43,7 +52,8 @@ func TestAuthenticateAdminCredentials(t *testing.T) {
 			AdminName: "admin",
 			AdminKey:  "12345edcba",
 		},
-		Storage: NullStorage{},
+		Storage:     NullStorage{},
+		AuthService: NullAuthService{},
 	}
 
 	a, err := Authenticate(c, w, r)
@@ -62,7 +72,8 @@ func TestAuthenticateAdminCredentials(t *testing.T) {
 func TestAuthenticateUnknownAccount(t *testing.T) {
 	r, w := setupAuthRecorder(t, "wrong", "1234512345")
 	c := &Context{
-		Storage: NullStorage{},
+		Storage:     NullStorage{},
+		AuthService: NullAuthService{},
 	}
 
 	_, err := Authenticate(c, w, r)
@@ -75,4 +86,24 @@ func TestAuthenticateUnknownAccount(t *testing.T) {
 		Message: "Unable to authenticate account [wrong]",
 		Retry:   false,
 	})
+}
+
+func TestAuthenticateNonAdminAccount(t *testing.T) {
+	r, w := setupAuthRecorder(t, "nonadmin", "1234512345")
+	c := &Context{
+		Storage:     NullStorage{},
+		AuthService: TrustingAuthService{},
+	}
+
+	a, err := Authenticate(c, w, r)
+	if err != nil {
+		t.Errorf("Unable to authenticate: %v", err)
+	}
+
+	if a.Name != "nonadmin" {
+		t.Errorf("Unexpected account name: %s", a.Name)
+	}
+	if a.Admin {
+		t.Errorf("Expected account not to be an administrator")
+	}
 }
