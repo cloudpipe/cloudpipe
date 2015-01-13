c73ed777ac3552cf47d369ae04829d5dc76640ee
Ash Wilson <smashwilson@gmail.com>
2015-01-13 08:37:02 -0500

    API endpoint to discover auth information.

---
+++ api_auth_test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestDiscoverAuthService(t *testing.T) {
+	r, err := http.NewRequest("GET", "https://localhost/v1/auth_service", nil)
+	if err != nil {
+		t.Fatalf("Unable to create request: %v", err)
+	}
+	w := httptest.NewRecorder()
+	c := &Context{
+		Settings: Settings{AuthService: "https://somewhere.com/"},
+	}
+
+	AuthDiscoverHandler(c, w, r)
+
+	if w.Code != http.StatusOK {
+		t.Errorf("Unexpected HTTP status: [%d]", w.Code)
+	}
+
+	var response struct {
+		Address string `json:"address"`
+		Style   string `json:"style"`
+	}
+	out := w.Body.Bytes()
+	if err := json.Unmarshal(out, &response); err != nil {
+		t.Fatalf("Unable to parse response body as JSON: [%s]", string(out))
+	}
+
+	if response.Address != "https://somewhere.com/" {
+		t.Errorf("Unexpected auth service address: %s", response.Address)
+	}
+	if response.Style != "local" {
+		t.Errorf("Unexpected auth service style: %s", response.Style)
+	}
+}
